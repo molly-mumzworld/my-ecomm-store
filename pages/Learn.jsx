c6e45abe0eb7dffd52b7677e9cb94ab5c53f6b5e
Mulham Bakeer <mulhambakeer@Mulhams-MacBook-Pro-2.local>
2022-06-17 03:19:33 +0400

    adding header

---
+++ pages/Learn.jsx
@@ -0,0 +1,6 @@
+
+const rootElement = document.getElementById('root')
+const element = document.createElement('div')
+element.textContent = 'Hello world'
+element.className = 'container'
+rootElement.appendChild(element)
