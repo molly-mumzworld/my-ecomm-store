efba3557c74a871a6b0a8011e8fb37356d1046b2
Mulham Bakeer <mulhambakeer@Mulhams-MacBook-Pro-2.local>
2022-06-17 01:57:07 +0400

    updating files

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           {products.map((product) => (
             <a key={product.id} href={product.href} className="group">
               <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
+                <Image
                   src={product.imageSrc}
                   alt={product.imageAlt}
                   className="w-full h-full object-center object-cover group-hover:opacity-75"
